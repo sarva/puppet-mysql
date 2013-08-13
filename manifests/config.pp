b96cb14f5928b5c9296e26384210c7e45fcc48bc
Mickaël Canévet <mickael.canevet@camptocamp.com>
2013-08-13 11:08:16 +0200

    Lint errors

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,24 +1,21 @@
-/*
-
-== Definition: mysql::config
-
-Set mysql configuration parameters
-
-Parameters:
-- *value*: the value to be set, defaults to the empty string;
-- *key*: optionally, set the key (defaults to $name);
-- *ensure*: defaults to present.
-
-Example usage:
-  mysql::config {'mysqld/pid-file':
-    ensure  => present,
-    value   => '/var/run/mysqld/mysqld.pid',
-  }
-
-If the section (e.g. 'mysqld/') is ommitted in the resource name,
-it defaults to 'mysqld/'.
-
-*/
+# == Definition: mysql::config
+#
+# Set mysql configuration parameters
+#
+# Parameters:
+# - *value*: the value to be set, defaults to the empty string;
+# - *key*: optionally, set the key (defaults to $name);
+# - *ensure*: defaults to present.
+#
+# Example usage:
+#   mysql::config {'mysqld/pid-file':
+#     ensure  => present,
+#     value   => '/var/run/mysqld/mysqld.pid',
+#   }
+#
+# If the section (e.g. 'mysqld/') is ommitted in the resource name,
+# it defaults to 'mysqld/'.
+#
 define mysql::config (
   $ensure='present',
   $value='',
@@ -30,7 +27,7 @@ define mysql::config (
     default => $key,
   }
 
-  $real_key = inline_template("<%= real_name.split('/')[-1] %>")
+  $real_key = inline_template('<%= real_name.split('/')[-1] %>')
 
   $section = inline_template("<%= if real_name.split('/')[-2]
       real_name.split('/')[-2]
@@ -51,15 +48,17 @@ define mysql::config (
   }
 
   augeas { "my.cnf/${section}/${name}":
-    incl      => $mysql::params::mycnf,
-    lens      => 'MySQL.lns',
-    changes   => [
+    incl    => $mysql::params::mycnf,
+    lens    => 'MySQL.lns',
+    changes => [
       "set target[.='${section}'] ${section}",
       $changes,
-      "rm target[count(*)=0]",
+      'rm target[count(*)=0]',
       ],
-    require   => [ File["${mysql::params::mycnf}"],
-                   File["${mysql::params::data_dir}"] ],
-    notify    => Service["mysql"],
+    require => [
+      File[$mysql::params::mycnf],
+      File[$mysql::params::data_dir],
+      ],
+    notify  => Service['mysql'],
   }
 }
