05bd2cf45d62155e1641861617018a73b973ae61
Sarva Bryant <sarva@graviteklabs.com>
2014-02-13 06:46:11 +0000

    update mysql configs

--- lib/facter/mysql_password.rb
+++
@@ -1,7 +0,0 @@
-Facter.add("mysql_password") do
-    ENV["PATH"]="/bin:/sbin:/usr/bin:/usr/sbin"
-    
-    setcode do
-        %x"test -f /root/.my.cnf && cat /root/.my.cnf | grep 'password' | head -n 1 | awk -F'=' '{print $2}'".strip
-    end
-end
--- manifests/config/mysqld.pp
+++ manifests/config/mysqld.pp
@@ -1,10 +1,24 @@
 class mysql::config::mysqld {
   mysql::config {
+    'net_read_timeout':     value => '7200',
+    'net_write_timeout':    value => '7200',
+    'bind-address':         value => '0.0.0.0';
     'pid-file':             value => '/var/run/mysqld/mysqld.pid';
     'old_passwords':        value => '0';
     'character-set-server': value => 'utf8';
     'log-warnings':         value => '1';
     'datadir':              value => "${mysql::params::data_dir}";
+    'max_allowed_packet':   value => '26M',
+    'innodb_file_per_table':value => '1',
+    'innodb_flush_log_at_trx_commit': value => '0',
+    'innodb_flush_method': value => 'O_DSYNC',
+    'innodb_buffer_pool_size': value => '32M',
+    'innodb_log_buffer_size': value => '8M',
+    'innodb_doublewrite': value => '0',
+    'innodb_thread_concurrency': value => '4',
+    'innodb_open_files': value => '150',
+    'transaction-isolation': value => 'READ-UNCOMMITTED',
+    'binlog_format': value => 'row',
     'log-error':            value => $::operatingsystem ? {
       /RedHat|Fedora|CentOS/ => '/var/log/mysqld.log',
       default                => '/var/log/mysql.err',
